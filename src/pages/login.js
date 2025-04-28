050c6dd2e5a5e069855cadbc0fb94162315dea24
Giampiero Barbaro <85945128+LordGiampiendos@users.noreply.github.com>
2025-04-28 14:54:48 +0200

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -31,7 +31,7 @@ function Login() {
         }),
         onSubmit: (values) => {
           setIsLoading(true);
-          axios.post('https://serverbarbaropersonal.pagekite.me/users/login', values, { headers: { 'WebRequest': 'aut' }, withCredentials: true})
+          axios.post('https://serverbarbaropersonal.pagekite.me/users/login', values, {withCredentials: true})
             .then(response => {
               login(response.data);
               navigate('/');
@@ -118,7 +118,6 @@ function Login() {
                       onSuccess={(values) => 
                         axios.post('https://serverbarbaropersonal.pagekite.me/users/google-login',  JSON.stringify({ tokenId: values.credential }), {
                           headers: {
-                            'WebRequest': true,
                             'Content-Type': 'application/json',
                           },  
                           withCredentials: true
